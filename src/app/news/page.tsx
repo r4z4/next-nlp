ef531495a5e06808ade07dc82965cfec8d4914ab
r4z4 <r4z4aa@gmail.com>
2023-06-25 00:20:25 +0000

    mdx is pretty cool, db coming along

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import Article, { BlogTitle } from './run_02.mdx';
+import Md from '../assets/md/glove/run_03.md'
+import Mdx from '../assets/mdx/glove/run_04.mdx'
+import ArticleImage from '../assets/article_images/glove/run_04.png'
 
 export default async function NewsArticle() {
 
     return (
         <div>
             <p>Hey</p>
-            <Article />;
+            <Mdx />;
             <BlogTitle />;
         </div>
     )
