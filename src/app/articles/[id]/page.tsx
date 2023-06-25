1e55cf47e47e9d98b9adf404b4d76731c5678e8f
r4z4 <r4z4aa@gmail.com>
2023-06-25 05:07:12 +0000

    syntax highlight

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,32 +1,59 @@
 "use client"
-import Mdx from '../../assets/md/glove/run_04.mdx'
 import {Glove_01, Glove_02, Glove_03, Glove_04} from '../../assets/mdx/glove/'
-import ArticleImage from '../assets/article_images/glove/run_04.png'
+import remarkGfm from 'remark-gfm'
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import python from 'react-syntax-highlighter/dist/esm/languages/prism/python';
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import mdxStyles from '../../components/Mdx.module.css'
+
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+SyntaxHighlighter.registerLanguage('javascript', js);
+SyntaxHighlighter.registerLanguage('python', python);
 
 type MapType = { 
     [id: string]: string; 
 }
 
-export default async function MdxArticle({
-    params,
-}: {
-    params: {id: number };
-}) {
-    console.log(`id=${params.id}`)
+interface CodeFnProps {
+    className: string
+    children: string
+}
+
+export default async function MdxArticle(id: number) {
+    function code({className, children, ...props}: CodeFnProps) {
+        const match = /language-(\w+)/.exec(className || '')
+        return match
+          ? <SyntaxHighlighter 
+                remarkPlugins={[[remarkGfm, {tableCellPadding: true, tablePipeAlign: true}]]} 
+                children={String(children).replace(/\n$/, '')} 
+                language={match[1]} 
+                style={dark} 
+                wrapLines={true}
+                PreTag="div" 
+                {...props} 
+            />
+          : <code className={className} {...props} />
+      }
     function getMdx(id: number) {
+        
         if (id == 1) {
-            return <Glove_03 />
+            return <Glove_01 className={mdxStyles.reactMarkDown} components={{code}} />
         } else if (id == 2) {
-            return <Glove_04 />
+            return <Glove_02 className={mdxStyles.reactMarkDown} components={{code}} />
+        } else if (id == 3) {
+            return <Glove_03 className={mdxStyles.reactMarkDown} components={{code}} />
+        } else if (id == 4) {
+            return <Glove_04 className={mdxStyles.reactMarkDown} components={{code}} />
         } else {
-            return <Glove_04 />
+            return <Glove_04 className={mdxStyles.reactMarkDown} components={{code}} />
         }
     }
-    const mdx = () => getMdx(params.id)
+
     return (
-        <div>
-            <p>Hey</p>
-            {getMdx(params.id)}
+        <div className={mdxStyles.reactMarkDown}>
+            {getMdx(id)}
         </div>
     )
 }
--- src/app/assets/mdx/glove/index.ts
+++ src/app/assets/mdx/glove/index.ts
@@ -1,4 +1,4 @@
-export { default as Glove_01 } from "./run_01.md";
-export { default as Glove_02 } from "./run_02.md";
+export { default as Glove_01 } from "./run_01.mdx";
+export { default as Glove_02 } from "./run_02.mdx";
 export { default as Glove_03 } from "./run_03.mdx";
 export { default as Glove_04 } from "./run_04.mdx";
